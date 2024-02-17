6ea6329df456ab9490659e20ca97ff9c67030eb8
Orhan Secilmis <66383550+Ghostfear1713@users.noreply.github.com>
2024-02-17 19:35:11 +0100

    Hele Unicorn opgaven

---
+++ src/main/java/org/example/Unicorn.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Unicorn {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int iD;
+private String name;
+private int age;
+private double powerStrength;
+
+    public Unicorn(String name, int age, double powerStrength) {
+        this.name = name;
+        this.age = age;
+        this.powerStrength = powerStrength;
+    }
+}
