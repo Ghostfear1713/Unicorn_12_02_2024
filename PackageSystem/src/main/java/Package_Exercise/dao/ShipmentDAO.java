ee07c567fca4701accca3afb49f02fd1b9f94378
Orhan Secilmis <66383550+Ghostfear1713@users.noreply.github.com>
2024-02-25 20:54:54 +0100

    DAO updated

---
+++ PackageSystem/src/main/java/Package_Exercise/dao/ShipmentDAO.java
@@ -0,0 +1,64 @@
+package Package_Exercise.dao;
+
+import Package_Exercise.config.HibernateConfig;
+import Package_Exercise.model.Location;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class ShipmentDAO {
+
+    EntityManagerFactory entityManagerFactory = HibernateConfig.getEntityManagerFactoryConfig();
+
+    public Location createLocation(Location location){
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(location);
+        em.getTransaction().commit();
+        em.close();
+
+        System.out.println("Your location " + location + "has now been added to the Database");
+        return location;
+    }
+
+    public void deleteLocation(int id) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        Location location = em.find(Location.class, id);
+        em.remove(location);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public Location updateLocation(Location location) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        Location updatedLocation = em.merge(location);
+        em.getTransaction().commit();
+        em.close();
+
+        System.out.println("Your location " + location + "has now been updated in the Database");
+        return updatedLocation;
+    }
+
+    public Location findLocation(int id) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        Location location = em.find(Location.class, id);
+        em.close();
+
+        return location;
+    }
+
+    public void getAllLocations() {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.createQuery("SELECT l FROM Location l", Location.class).getResultList().forEach(System.out::println);
+    }
+
+
+
+
+
+
+
+
+
+}
